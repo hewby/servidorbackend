150bf7756805d6c63fb282ae06002dbcc13f5476
hewby <vitorhckimura@gmail.com>
2025-07-14 20:42:41 -0300

    server.py

--- server.py
+++ server.py
@@ -39,4 +39,4 @@ def limpar_invalidos():
     return jsonify({"mensagem": "Pedidos inválidos foram removidos."}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    #app.run(host="0.0.0.0", port=10000)
